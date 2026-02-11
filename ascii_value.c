658bfb2064e71c14dcf395221f8ae8ecd3ae78da
bhagyrajsinh <bhagyrajsinh561@gmail.com>
2026-02-11 19:54:54 +0530

    Add files via upload

---
+++ ascii_value.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+
+{
+
+char letter;
+int ascii;
+
+
+printf("please enter letter :-");
+scanf("%c",&letter);
+
+
+ascii = letter;
+printf("ascii = %d",ascii);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ ascii_value_2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+
+{
+
+char letter;
+int ascii;
+
+
+printf("please enter ascii :-");
+scanf("%d",&ascii);
+
+
+ letter=ascii ;
+printf("letter = %c",letter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

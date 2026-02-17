4fbc6b2c8d4a3f6e32cbc72facb7c76a187ab93e
bhagyrajsinh <bhagyrajsinh561@gmail.com>
2026-02-17 06:56:25 +0530

    17/02/2026

---
+++ 8_if.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main()
+
+{
+
+float length,widgth;
+
+printf("Please enter length");
+scanf("%f",&length);
+
+printf("Please enter widgth");
+scanf("%f",&widgth);
+
+if(length==widgth);
+{
+printf("shape is square");
+}
+
+if (length>widgth)
+{
+    printf("shape is portrait" );
+
+}
+
+if(length<widgth)
+{
+    printf("shape is landscape");
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

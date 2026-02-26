ef75f3cc9f12c5b9015618399cdb6e3307187017
bhagyrajsinh <bhagyrajsinh561@gmail.com>
2026-02-26 07:32:38 +0530

    Add files via upload

--- 17_while_loop5.c
+++ 17_while_loop5.c
@@ -2,18 +2,42 @@
 void main()
 {
 int year,count;
-float amount,intrest;
+float amount,interest,rate;
 
-amount=80000;
-intrest=10;
-year=5;
+printf("Entre amount ");
+scanf("%f",&amount);
+
+printf("Enter rate");
+scanf("%f",&rate);
+
+printf("Enter year ");
+scanf("%d",&year);
+
+count=0;
+//while(count<year)
+//amount=1000;
+//rate=10;
+//year=5;
+while(count<year)
+{
+interest= (amount*rate*1)/100;
+amount=amount+interest; 
+count++;
+}
+  
+//interest= (amount*rate*1)/100;
+//amount=amount+interest; 
+
+//interest= (amount*rate*1)/100;
+//amount=amount+interest; 
+
+//interest= (amount*interest*1)/100;
+//amount=amount+intrest; 
+
+//interest= (amount*interest*1)/100;
+//amount=amount+interest; 
 
 
-intrest= (amount*intrest*1)/100;
-amount=amount+intrest;
 
-intrest= (amount*intrest*1)/100;
-amount=amount+intrest;
-
-printf("intrest = %.2f",intrest);
+printf("amount = %.2f ",amount);
 }

f2461fead6fa18a5329ce4c7d5f3f1ea097fb7f0
bhagyrajsinh <bhagyrajsinh561@gmail.com>
2026-03-13 20:08:36 +0530

    Add files via upload

---
+++ inner_loop.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int line,count;
+for (count=0;count<=5;count++)
+{
+for(line=1;line<=count;line++)
+{
+
+printf("*");
+}
+printf("\n");
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
